7fdabb547f538693ce36704590a503e7cab908fd
Daniel Lo Nigro <daniel@dan.cx>
2016-10-16 12:09:36 -0700

    `git add -A` so new files are added too

--- update-and-push.sh
+++ update-and-push.sh
@@ -10,5 +10,6 @@ if [ $? -ne 0 ]; then
 fi;
 
 LATEST_VERSION=`curl --fail https://yarnpkg.com/latest-version`
+git add -A
 git commit -a -m "Automated upgrade to Yarn $LATEST_VERSION" --author='DanBuild <build@dan.cx>'
 git push origin
