62b380cc54f8e9e92e9729c1dfaf7f7fa8be7e7a
Daniel Lo Nigro <daniel@dan.cx>
2016-11-15 22:44:16 -0800

    Update update.sh to use Aptly

--- debian-source/add-deb.sh
+++ debian-source/add-deb.sh
@@ -7,22 +7,17 @@
 
 set -ex
 
-debian_source_path="$(dirname $(readlink --canonicalize-existing $0))"
-debian_output="$debian_source_path/../debian"
+./update-aptly-config.sh
 
 # Aptly doesn't support publishing into a custom directory, so we do some hacks:
 # Move the public directory to where Aptly expects it, publish, then move it
 # back to our preferred location at the end.
-mkdir -p $debian_source_path/public/
-mv $debian_output/* $debian_source_path/public/
-
-# Aptly doesn't support passing root directory as argument, so we need to edit
-# it in the config.
-sed -i "s~rootDir.*~rootDir\": \"$debian_source_path\",~" $debian_source_path/.aptly.conf
+mkdir -p public/
+mv ../debian/* public/
 
 # Add the package to the repo and publish the changes
 aptly -config=./.aptly.conf repo add yarn $1
 aptly -config=./.aptly.conf publish update -gpg-key=9D41F3C3 stable
 
 # Move the public files back to the right place
-mv $debian_source_path/public/* $debian_output/
+mv public/* ../debian/
