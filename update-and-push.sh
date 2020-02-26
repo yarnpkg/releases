d93c11c5ca89ef117ba930904c72e8b78e4385ec
Daniel Lo Nigro <d@d.sb>
2020-02-26 15:19:29 -0800

    Updated another old URL to go to classic.yarnpkg.com

--- update-and-push.sh
+++ update-and-push.sh
@@ -10,7 +10,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi;
 
-LATEST_VERSION=`curl --fail https://yarnpkg.com/latest-version`
+LATEST_VERSION=`curl --fail https://classic.yarnpkg.com/latest-version`
 git add -A
 git commit -a -m "Automated upgrade to Yarn $LATEST_VERSION" --author='DanBuild <build@dan.cx>'
 git push origin
